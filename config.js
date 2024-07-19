dfc0700b79483a80c793889ff3398fcfe118b8b9
Shafi Ul Hussnain <106338425+hussnainshafiul@users.noreply.github.com>
2024-07-19 13:33:13 +0500

    Update config.js

--- config.js
+++ config.js
@@ -8,13 +8,7 @@ import 'firebase/compat/firestore'
 
 const firebaseConfig = {
 
-  apiKey: "AIzaSyCll5gksi9TqSW5emuJztlwsteyKhRzcxA",
-  authDomain: "test-bcc09.firebaseapp.com",
-  projectId: "test-bcc09",
-  storageBucket: "test-bcc09.appspot.com",
-  messagingSenderId: "864525403155",
-  appId: "1:864525403155:web:251c3877dc9437d1926be6",
-  measurementId: "G-LSSFGJ0HSZ"
+//
 }
 
 if (!firebase.apps.length)
